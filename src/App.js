0f5dbf09900185f6abfb173253bbb5557bd8e2a2
MacBookPro <t-saito@MacBookPronoMacBook-Pro.local>
2020-03-15 15:07:22 +0900

    HTTP通信の実装、axiosの導入

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 ReactDOM.render(
     React.createElement(
@@ -10,10 +11,45 @@ ReactDOM.render(
     document.getElementById('content')
 );
 
-class Greeting extends React.Component{
-    render(){
-        return <h1>Hi</h1>;
-    }
+const server = 'http://localhost:8000/api/sample';
+ 
+class Greeting extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      result: '',
+    };
+ 
+    this.handleClick = this.handleClick.bind(this);
+  }
+ 
+  handleClick(event) {
+    axios.get(server)
+      .then((res) => {
+        this.setState({
+          status: true,
+          result: res.data
+        });
+        console.log(this.state);
+      })
+      .catch((e) => {
+        console.error(e);
+        this.setState({
+          status: false,
+          result: e,
+        });
+      });
+  }
+  render() {
+    const result = (this.state.status) ? (<div>{this.state.result}</div>) : (<div>Not Yet</div>);
+    return (
+      <div>
+        <button onClick={this.handleClick}>Get Data</button>
+        {result}
+      </div>
+    );
+  }
 }
 
 export default Greeting;
